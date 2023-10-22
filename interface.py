9118e72c85da591f1c6eac0ef293a0b953018870
root <root@localhost.localdomain>
2023-10-22 13:43:46 +0800

    logic gates here we go

--- interface.py
+++ interface.py
@@ -2,7 +2,9 @@ from task import Task
 #please add a condition to make way for program termination
 class Interface:
     def __init__(self, pseudoDB : object, timer : object):
-        self.__isAtAddingWorkLoad = True
+        self.__isTerminated = False
+        self.__isAtHomeMenu = True
+        self.__isAtAddingWorkLoad = False
         self.__isWorking = False
         self.__isCheckingWorkProgress = False
         self.__timer = timer
@@ -11,25 +13,41 @@ class Interface:
     
     #state checker
     def machineState(self) -> str:
+        stateSignals = { (True, False, False, False, False) : "terminated",
+                         (False, True, False, False, False) : "at_home_menu",
+                         (False, False, True, False, False) : "adding_work",
+                         (False, False, False, True, False) : "working",
+                         (False, False, False, False, True) : "checking_progress"
+                       }
         
-        if self.__isAtAddingWorkLoad == True and self.__isWorking == False and self.__isCheckingWorkProgress == False:
-            self.__currentState = "adding_workload"
-        elif self.__isWorking == True and self.__isAtAddingWorkLoad == False and self.__isCheckingWorkProgress == False:
-            self.__currentState = "working"
-        elif self.__isCheckingWorkProgress == True and self.__isAtAddingWorkLoad == False and self.__isWorking == False:
-            self.__currentState = "checking_progress"
-        
+        for signals, state in stateSignals.items():
+            if signals == (self.__isTerminated, self.__isAtHomeMenu, self.__isAtAddingWorkLoad, self.__isWorking, self.__isCheckingWorkProgress):
+                self.__currentState = state
+                break
         return self.__currentState
     
     #state modifiers
-    def resetStateTransition(self) -> str:
-        self.__currentState = "adding_workload"
-        self.__isAtAddingWorkLoad = True
+    def resetStateTransition(self) -> bool:
+        self.__isTerminated = False
+        self.__isAtHomeMenu = True
+        self.__isAtAddingWorkLoad = False
         self.__isWorking = False
         self.__isCheckingWorkProgress = False
 
-        return self.__currentState
+        return self.__isAtHomeMenu
 
+    def atHomeMenu(self):
+        while self.__isAtHomeMenu == True:
+            ask = input("wanna be productive [y/n]: ").lower()
+            
+            if ask == "n":
+                self.__isAtHomeMenu = False
+                self.__isTerminated = True
+            elif ask == "y":
+                self.__isAtAddingWorkLoad = True
+
+        return self.__isAtHomeMenu 
+        
     def atAddingWorkLoad(self) -> bool:
         while self.__isAtAddingWorkLoad == True:
             ask = input("add work [y/n]: ").lower()
@@ -46,7 +64,7 @@ class Interface:
         return self.__isAtAddingWorkLoad 
         
   
-    def atWorkingProcess(self) -> str:
+    def atWorkingProcess(self) -> bool:
         somethingToDo = len(self.__pseudoDB.getPending()) > 0
 
         if somethingToDo:
--- main.py
+++ main.py
@@ -2,23 +2,21 @@ from interface import Interface
 from storage import Storage
 from task import Task
 from timer import Timer 
+from enum import Enum
+
 
 if __name__ == "__main__":
-    states = ("adding_workload", "working", "checking_progress")
     task = Task()
     timer = Timer()
     storage = Storage()
     menu = Interface(storage,timer)
 
+    states = ("terminated", "at_home_menu" ,"adding_workload", "working", "checking_progress")
     #dagdag ka ng termination sa states para mag terminate ang program
     #start ka na mag implement ng file handling
     while True:
         if menu.machineState() == states[0]:
-            menu.atAddingWorkLoad()
+            break
         elif menu.machineState() == states[1]:
-            menu.atWorkingProcess()
-        elif menu.machineState() == states[2]:
-            menu.atCheckingProgress()
-            menu.resetStateTransition()
-        
-        
+            menu.atHomeMenu()
+       
