f912baac6ae90a7d7a5f79521800ff2b7387352c
jg-manalo <130198713+jg-manalo@users.noreply.github.com>
2023-10-29 05:50:10 +0800

    Implemented Switch Casing - much better and readable if it is implemented rather than if else statement

--- main.py
+++ main.py
@@ -22,28 +22,33 @@ if __name__ == "__main__":
 
     while isProgramRunning:
         menuStatus = menu.machineState()
-        if menuStatus == MachineState.HOME_MENU:
-            menu.atHomeMenu()
-            clearScreen()
-        elif menuStatus == MachineState.ADDING_WORKLOAD:
-            menu.atAddingWorkLoad()
-            clearScreen()
-        elif menuStatus == MachineState.WORK_SELECTION:
-            menu.atWorkSelectionProcess()
-        elif menuStatus == MachineState.WORKING:
-            menu.atWorkingProcess()
-            time.sleep(5)
-            clearScreen()
-        elif menuStatus == MachineState.CHECKING_PROGRESS:
-            menu.atCheckingProgress()
-            clearScreen()
-        elif menuStatus == MachineState.RETRYING_TASK:
-            menu.atRetryingState()
-        elif menuStatus == MachineState.TERMINATED:
-            menu.cleanUp()
-            menu.closeFile()
-            isProgramRunning = False
-        else:
-            menu.resetState()
-            clearScreen()
+
+        match menuStatus:
+            case MachineState.HOME_MENU:
+                menu.atHomeMenu()
+                clearScreen()
+                break
+            case MachineState.ADDING_WORKLOAD:
+                menu.atAddingWorkLoad()
+                clearScreen()
+                break
+            case MachineState.WORK_SELECTION:
+                menu.atWorkSelectionProcess()
+                clearScreen()
+                break
+            case MachineState.WORKING:
+                menu.atWorkingProcess()
+                time.sleep(5)
+                clearScreen()
+                break
+            case MachineState.CHECKING_PROGRESS:
+                menu.atCheckingProgress()
+                clearScreen()
+                break
+            case MachineState.TERMINATED:
+                menu.cleanUp()
+                menu.closeFile()
+                isProgramRunning = False
+                break
+
         
